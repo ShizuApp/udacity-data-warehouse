f91eb10843e1479c59356e0a2b83f434abddc367
Eduardo Jimenez <eduardojimfan@gmail.com>
2022-07-18 15:56:11 -0400

    Add docstrings to project files

--- create_tables.py
+++ create_tables.py
@@ -1,30 +1,41 @@
+"""
+Import SQL queries from sql_queries, connect to host and run
+queries to create the tables
+"""
+
 import configparser
 import psycopg2
 from sql_queries import create_table_queries, drop_table_queries
 
 
 def drop_tables(cur, conn):
+    # Run queries to drop existing tables
     for query in drop_table_queries:
         cur.execute(query)
         conn.commit()
 
 
 def create_tables(cur, conn):
+    # Run queries to create the tables
     for query in create_table_queries:
         cur.execute(query)
         conn.commit()
 
 
 def main():
+    # Read config file
     config = configparser.ConfigParser()
     config.read('dwh.cfg')
 
+    # Open a connection to the host
     conn = psycopg2.connect("host={} dbname={} user={} password={} port={}".format(*config['CLUSTER'].values()))
     cur = conn.cursor()
-
+    
+    # Run SQL queries
     drop_tables(cur, conn)
     create_tables(cur, conn)
 
+    # Close connection
     conn.close()
 
 
--- etl.py
+++ etl.py
@@ -1,30 +1,42 @@
+"""
+Import SQL queries from sql_queries, connect to host and run
+queries to transfer the data and fill tables 
+"""
+
 import configparser
 import psycopg2
 from sql_queries import copy_table_queries, insert_table_queries
 
 
 def load_staging_tables(cur, conn):
+    # Run queries to copy data from S3 to staging tables
     for query in copy_table_queries:
         cur.execute(query)
         conn.commit()
 
 
 def insert_tables(cur, conn):
+    # Run queries to fill tables
     for query in insert_table_queries:
         cur.execute(query)
         conn.commit()
 
 
 def main():
+
+    # Read config file
     config = configparser.ConfigParser()
     config.read('dwh.cfg')
 
+    # Open a connection to the host
     conn = psycopg2.connect("host={} dbname={} user={} password={} port={}".format(*config['CLUSTER'].values()))
     cur = conn.cursor()
     
+    # Run SQL queries
     load_staging_tables(cur, conn)
     insert_tables(cur, conn)
 
+    # Close connection
     conn.close()
 
 
